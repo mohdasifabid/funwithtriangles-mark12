25f3ac82dc9b2f993b7824516d7aab6d1fbbbc90
asif <masifabid@outlook.com>
2021-08-26 02:29:34 +0530

    problem of score is resolved

--- quiz.js
+++ quiz.js
@@ -8,48 +8,39 @@ const rightAnswer = ["90", "right angled"];
 const output = document.querySelector("#output")
 
 
-quizForm.addEventListener("submit",function(e){
+
+
+quizForm.addEventListener("submit", function(e){
     e.preventDefault()
-    let score = 0;
+    
+
     const question1 = document.getElementsByName("question1")
     const question2 = document.getElementsByName("question2")
+
+    var score = 0;
+
     for(let i=0; i<question1.length; i++){
-        if(question1[i].checked){
-            if(question1[i].value ===rightAnswer[0]){
-                score = score +1; 
-                
-            }
-              } 
-              
-    }
-    // question1.forEach(item => {
-    //     if(item.checked){
-    //         if(item.value ===rightAnswer[0]){
-    //             score++
-    //         }
-    //     }
-    // })
-    // question2.forEach(item => {
-    //     if(item.checked){
-    //         if(item.value ===rightAnswer[1]){
-    //             score++
-    //         }
-    //     }
-    // })
-    // console.log("your score is: ",score)
-    
-    for(let i=0; i<question2.length; i++) {
-        if(question2[i].checked) {
-            if(question2[i].vlaue===rightAnswer[0]){
-                score = score +1;
-                
+                if(question1[i].checked){
+                    if(question1[i].value === rightAnswer[0]){
+                        score = score +1; 
+                        
+                    }
+                      } 
+                      
             }
 
-        }
+            for(let i=0; i<question2.length; i++){
+                if(question2[i].checked){
+                    if(question2[i].value === rightAnswer[1]){
+                        score = score +1; 
+                        
+                    }
+                      } 
+                      
+            }
+            console.log(score)
+    output.textContent = `Your score is:  ${score}`
 
-    }
-
-    output.textContent = "You did great! and your score is " + score;
-});
+})
 
 
